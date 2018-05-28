71f98b244ce6b9223d6164c1761334940a630122
Luc Duron <duron.luke@gmail.com>
2018-05-28 19:17:46 +0200

    Add densify_profiles

---
+++ densify_profils.py
@@ -0,0 +1,40 @@
+"""
+Densifier des profils en travers
+"""
+import time
+
+from core.arg_command_line import MyArgParse
+from core.utils import get_axe_hydraulique
+from core.base import LigneContrainte, MeshConstructor, SuiteProfilsTravers
+
+
+def main(args):
+    t1 = time.clock()
+
+    print("~> Lecture des fichiers d'entrées")
+    axe = get_axe_hydraulique(args.infile_axe)
+    profils_travers = SuiteProfilsTravers(args.infile_profils_travers, "Profils en travers", field=args.attr_profils_travers)
+    lignes_contraintes = LigneContrainte.get_lines_from_file(args.infile_lignes_contraintes)
+
+    profils_travers.compute_dist_proj_axe(axe)
+    profils_travers.find_and_add_limits(lignes_contraintes, args.dist_max)
+
+    profils_travers.check_intersections()
+    profils_travers.sort_by_dist()
+
+    mesh_constr = MeshConstructor(profils_travers, args.pas_trans)
+    mesh_constr.build_interp(lignes_contraintes, args.pas_long, True)
+    mesh_constr.export_profiles(args.out_profiles)
+
+    t2 = time.clock()
+    print("=> le temps d'execution est de : {}s".format(t2-t1))
+
+
+if __name__ == '__main__':
+    parser = MyArgParse(description=__doc__)
+    parser.add_common_args()
+    args = parser.parse_args()
+    # Outputs
+    parser.add_argument("outfile_profiles", help="fichier de sortie contenant les profils en travers (i3s)")
+
+    main(args)
