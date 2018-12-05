95040ac10d42f49c471abf2d5ff4a88fc6ead7fa
Luc Duron <duron.luke@gmail.com>
2018-12-05 15:05:01 +0100

    Support all branches with geometry

--- mesh_crue10_submodel_bathy.py
+++ mesh_crue10_submodel_bathy.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """
 Génération d'une surface 2D à partir de la géométrie d'un sous-modèle Crue10
-/!\ Pour l'instant seuls les sections profil des branches Saint-Venant sont considérées et sans prise en compte des limites de lits!
+
+Branches: seules les branches avec des SectionProfil et/ou SectionIdem sont traitées :
+- EMHBrancheSaintVenant (type 20)
+- EMHBrancheSeuilTransversal (type 4)
+- EMHBrancheStrickler (type 6)
+- EMHBarrageFilEau (type 15)
+L'option `--types_branches` permet de sélectionner les types de branches à traiter.
+
+Particularités :
+- SectionProfil: les sections situées aux extrémités (amont/aval) des branches sont replacés pour correspondre
+    géographiquement au début/fin de la branche
+- SectionIdem:
+    - l'abscisse curviligne xp est normalisée (par la géométrie)
+    - la traceProfil est copiée (si la SectionProfil originale est au même endroit) ou regénérée (perpendiculaire à la branche)
+- Les branches non actives sont également traitées
 """
+from crue10.emh.branche import Branche
 from crue10.emh.section import SectionProfil
 from crue10.emh.submodel import SubModel
 from crue10.utils import CrueError, logger
@@ -13,7 +28,6 @@ from core.arg_command_line import MyArgParse
 from core.base import LigneContrainte, MeshConstructor, SuiteProfilsTravers, ProfilTravers
 from core.utils import float_vars, logger, set_logger_level, TatooineException
 
-
 SHIFT_TRACE_TO_EXTERMITY = True
 
 
@@ -23,7 +37,7 @@ def mesh_crue10_submodel_bathy(args):
     # Read SubModel from xml/shp files
     try:
         model = SubModel(args.infile_etu, args.submodel_name)
-        model.read_drso()
+        model.read_drso(filter_branch_types=args.types_branches)
         model.read_dptg()
         model.read_shp_noeuds()
         model.read_shp_traces_sections()
@@ -35,10 +49,6 @@ def mesh_crue10_submodel_bathy(args):
         logger.critical(e)
         sys.exit(2)
     logger.info(model)
-
-    #TODO:
-    # - merge branches at transitions "SectionIdem/SectionProfil"
-    # - generate trace of ProfilIdem
 
     for branche in model.iter_on_branches():
         branche.normalize_sections_xp()
@@ -79,7 +89,7 @@ def mesh_crue10_submodel_bathy(args):
                     mesh_constr.build_mesh()
                     if len(mesh_constr.points) != len(mesh_constr.triangle['vertices']):
                         raise TatooineException("Mesh is corrupted... %i vs %i nodes" % (
-                                                len(mesh_constr.points), len(mesh_constr.triangle['vertices'])))
+                            len(mesh_constr.points), len(mesh_constr.triangle['vertices'])))
                     else:
                         if not triangles:  # set initial values from first iteration
                             points = mesh_constr.points
@@ -88,7 +98,8 @@ def mesh_crue10_submodel_bathy(args):
                         else:  # concatenate with current sub-mesh for next iterations
                             points = np.hstack((points, mesh_constr.points))
                             triangles['triangles'] = np.vstack(
-                                (triangles['triangles'], triangles['vertices'].shape[0] + mesh_constr.triangle['triangles']))
+                                (triangles['triangles'],
+                                 triangles['vertices'].shape[0] + mesh_constr.triangle['triangles']))
                             triangles['vertices'] = np.vstack((triangles['vertices'], mesh_constr.triangle['vertices']))
             else:
                 logger.info("Branche ignorée par manque de sections")
@@ -112,6 +123,12 @@ parser = MyArgParse(description=__doc__)
 parser_infiles = parser.add_argument_group("Données d'entrée du sous-modèle Crue10 à traiter")
 parser_infiles.add_argument("infile_etu", help="fichier d'étude Crue10 (etu.xml)")
 parser_infiles.add_argument("submodel_name", help="nom du sous-modèle")
+help_branche_types = "Type des branches à considérer (liste d'entiers) : "
+for i, id_type in enumerate(Branche.TYPES_WITH_GEOM):
+    if i != 0: help_branche_types += ', '
+    help_branche_types += '%i = %s' % (id_type, Branche.TYPES[id_type])
+parser_infiles.add_argument("--types_branches", help=help_branche_types, type=int, nargs='+',
+                            choices=Branche.TYPES_WITH_GEOM, default=[20])
 # Mesh parameters
 parser_mesh = parser.add_argument_group("Paramètres pour la génération du maillage 2D")
 parser_mesh.add_argument("--pas_long", type=float, help="pas d'interpolation longitudinal (en m)", default=5)
