6721ce122782b0c4b8f66a793132bf109861b783
Luc Duron <duron.luke@gmail.com>
2019-04-24 11:48:08 +0200

    Use perf_counter instead of deprecated clock function

--- densify_profiles.py
+++ densify_profiles.py
@@ -2,7 +2,7 @@
 """
 Densifier des profils en travers
 """
-import time
+from time import perf_counter
 
 from core.arg_command_line import MyArgParse
 from core.base import LigneContrainte, MeshConstructor, SuiteProfilsTravers
@@ -11,7 +11,7 @@ from core.utils import get_axe_hydraulique, logger, set_logger_level, TatooineEx
 
 def densify_profiles(args):
     set_logger_level(args.verbose)
-    t1 = time.clock()
+    t1 = perf_counter()
 
     logger.info("~> Lecture des fichiers d'entrées")
     axe = get_axe_hydraulique(args.infile_axe)
@@ -33,7 +33,7 @@ def densify_profiles(args):
     mesh_constr.build_interp(lignes_contraintes, args.pas_long, True)
     mesh_constr.export_profiles(args.outfile_profiles)
 
-    t2 = time.clock()
+    t2 = perf_counter()
     logger.info("=> le temps d'execution est de : {}s".format(t2-t1))
 
 
