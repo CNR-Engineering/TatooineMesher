87b66638bac2fb94a566bbc1b594a45af27bfcfc
Nicolas Godet <39594821+nicogodet@users.noreply.github.com>
2024-02-15 08:50:11 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from glob import glob
+from setuptools import find_packages, setup
+
+from tatooinemesher import VERSION
+
+
+EXCLUDE_FROM_PACKAGES = ['cli', 'tests']
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+cli_files = []
+for file in glob('cli/*.py'):
+    if not file.endswith('__init__.py'):
+        cli_files.append(file)
+
+setup(
+    name='TatooineMesher',
+    version=VERSION,
+    author='Luc Duron',
+    author_email='l.duron@cnr.tm.fr',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    scripts=cli_files,
+    install_requires=requirements,
+    description='Python library for Telemac post-processing tasks',
+    url='https://github.com/CNR-Engineering/TatooineMesher',
+)
